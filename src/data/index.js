2cbffa633483381e555273c3e6b1ebf6ad319cf1
dapent-react <dapengzhang.jobapplication@gmail.com>
2020-02-09 16:40:23 +1100

    Completed the data module and the testing file

--- src/data/index.js
+++ src/data/index.js
@@ -1,13 +1,27 @@
-export const getIphones = (searchTerm) => {
+export const getIphones = async (searchTerm, handleError) => {
   // use the fetch API to load iphones.json to iphones variable
-  const iphones = []
-  // after this has loaded call the filterIphones() function below to filter the results
-  return filterIphones(iphones, searchTerm)
+  try {
+    const response = await fetch('/iphones.json')
+    const iphones = await response.json()
+    // after this has loaded call the filterIphones() function below to filter the results
+    return filterIphones(iphones, searchTerm)
+  } catch (error) {
+    // error handling logics
+    if (handleError) handleError(error);
+    return [];
+  }
 }
 
+// do filtering
 export const filterIphones = (iphones, searchTerm) => {
-  return iphones.filter(iphone => { 
-    // filter the iphone
-    return iphone
+  return iphones.filter(iphone => {
+    if (exist (iphone['name'], searchTerm)) return true;
+    if (exist (iphone['color'], searchTerm)) return true;
+    if (exist (iphone['capacity'], searchTerm)) return true;
+    return false;
   }) 
 }
+// search logic - return true when a value contains a term
+// case insensitive
+const exist = (value, term) => 
+  String(value).toLowerCase().indexOf(String(term).toLowerCase()) > -1;
--- src/data/index.test.js
+++ src/data/index.test.js
@@ -1,11 +1,46 @@
-import { filterIphones } from './'
+import { filterIphones, getIphones } from './'
+
+const sampleData = [
+  {
+    "id": 1,
+    "name": "iPhone SE",
+    "color": "Silver",
+    "capacity": "16GB",
+    "price": 629
+  },
+  {
+    "id": 2,
+    "name": "iPhone SE",
+    "color": "Silver",
+    "capacity": "64GB",
+    "price": 829
+  },
+]
 
 describe('Data module', () => {
   describe('filterIphones', () => {
-    it('Should do something', () => {
-      expect(filterIphones([], '')).toEqual([
-        { foo: 'bar' }
-      ])
+    it(`Should return an empty array when the searchTerm does not exist
+    in name, color, or capacity of any iphone`, () => {
+      expect(filterIphones(sampleData, 'samsung')).toEqual([])
     })
+    it(`Should return an array of all iphones whose name, color, or capacity
+    contains the searTerm (case insensitive)`, () => {
+      expect(JSON.stringify(filterIphones(sampleData, 'silver'))).toEqual(
+        JSON.stringify(sampleData)
+      )
+      expect(JSON.stringify(filterIphones(sampleData, '16GB'))).toEqual(
+        JSON.stringify([sampleData[0]])
+      )
+    });
   })
+
+  describe('getIphones', () => {
+    it('Should call a handleError function when error occurs in fetching data', async () => {
+      const error = new Error('test')
+      window.fetch = jest.fn().mockRejectedValue(error);
+      const handleError = jest.fn();
+      await getIphones('', handleError);
+      expect(handleError).toHaveBeenCalledWith(error)
+    })
+  });
 })
